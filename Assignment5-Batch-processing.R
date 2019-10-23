c8d6d80888115bf79af181959bd1c913ae19cb7d
Pooja123-bit <poojaupadhyay100@gmail.com>
2019-10-23 15:57:45 -0500

    edit

--- Assignment5-Batch-processing.R
+++ Assignment5-Batch-processing.R
@@ -7,13 +7,12 @@ Batch.Files<-list.files("Batch.Files",full=TRUE,pattern="4")
 
 phy<-adply(.data=Batch.Files, .margins=1, function(file) {
   
-  #read the data
-  d<-read.table(file, sep="\t", skip=10, header=TRUE, fileEncoding="ISO-8859-1",
-                stringsAsFactors=FALSE, quote="\"",check.names=FALSE,
-                encoding="UTF-8",na.strings=9999.99)
-  
-  
-  #clean names
+  d<-read_table(file, col_names=T)
+  d = d[-1,]
+ 
+  d$COND=NULL
+  d$match=1:nrow(d)
+   
   head<-names(d)
   head<-str_replace(head, "\\(.*\\)", "")
   head<-str_trim(head)
@@ -21,15 +20,13 @@ phy<-adply(.data=Batch.Files, .margins=1, function(file) {
   head<-tolower(head)
   head<-str_replace(head,fixed(",."),".")
   
-  #assign names
   names(d) = head
   
-  #create a proper date time format
   date<-scan(file,what="character",skip=1,nlines=1,quiet=TRUE)
   
   d$date<-date[2]
   
-  d$dateTime<-str_c(d$date, d$ttime, sep=" ")
+  d$dateTime<-str_c(d$date, d$time, sep=" ")
   
   d$dateTime<-as.POSIXct(strptime(d$dateTime,format="%m/%d/%y %H:%M:%OS",
                                   tz="America/New_York"))
